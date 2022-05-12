fbb0005129b0c051101b4d02992d175ac7ec7947
ryutaro kudo <ryutaro.kudo.p2@dc.tohoku.ac.jp>
2022-05-12 13:51:23 +0900

    read_dumppos.pyでpd.read_csvを使って高速化

--- package/KudoCop/io/read_dumppos.py
+++ package/KudoCop/io/read_dumppos.py
@@ -7,36 +7,26 @@ class ReadDumppos():
         pass
 
     def read_file(self, sdat, ifn: str) -> None:
+        current_row = 0
         with open(ifn, 'r') as ifp:
-            lines = ifp.readlines()
+            while True:
+                current_row += 1
+                spline = ifp.readline().split()
+                if len(spline) == 0:
+                    continue
+                if spline[0] == "ITEM:" and spline[1] == "BOX":
+                    for dim in range(3):
+                        spline = ifp.readline().split()
+                        sdat.cell[dim] = float(spline[1])
+                    current_row += 3
+                    continue
+                if spline[0] == 'ITEM:' and spline[1] == 'ATOMS':
+                    columns = spline[3:]
+                    break
 
-        for ind, line in enumerate(lines):
-            spline = line.split()
-            if len(spline) == 0:
-                continue
-            if spline[0] == "ITEM:" and spline[1] == "NUMBER":
-                total_particle = int(lines[ind+1])
-
-            elif spline[0] == "ITEM:" and spline[1] == "BOX":
-                for dim in range(3):
-                    input_box = lines[ind+dim+1].split()
-                    sdat.cell[dim] = float(input_box[1])
-
-            elif spline[0] == "ITEM:" and spline[1] == "ATOMS":
-                spline = spline[2:]
-                atom_data = dict()
-                splines = np.array([l.split()
-                                   for l in lines[ind+1:ind+1+total_particle]])
-                for c in range(len(spline)):
-                    if spline[c] == 'id':
-                        # 0-indexed
-                        index = splines[:, c].astype(int) - 1
-                    elif spline[c] in ['type', 'mask']:
-                        atom_data[spline[c]] = splines[:, c].astype(int)
-                    else:
-                        atom_data[spline[c]] = splines[:, c].astype(float)
-
-                sdat.atoms = pd.DataFrame(data=atom_data, index=index)
-                sdat.atoms.sort_index(inplace=True)
-
-        sdat.atom_type_set |= set(sdat.atoms['type'])
+        skip_rows = current_row
+        sdat.atoms = pd.read_csv(
+            ifn, skiprows=skip_rows, sep=' ', names=columns)
+        sdat.atoms[['type', 'mask']] = sdat.atoms[['type', 'mask']].astype(int)
+        sdat.atoms.index = sdat.atoms.index - 1
+        sdat.atoms.sort_index(inplace=True)
