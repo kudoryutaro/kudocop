ce898c99be9814b5d5a94882ee9fadd558cab7c3
kudoryutaro <ryutaro.kudo.p2@dc.tohoku.ac.jp>
2022-09-13 15:54:12 +0900

    added get_coordination_number_distribution

--- package/KudoCop/analyze/analyze_atom.py
+++ package/KudoCop/analyze/analyze_atom.py
@@ -65,12 +65,12 @@ class AnalyzeAtom():
                 neibour_atom_type2 = sdat_atom_type[neibour_atom_idx2]
                 if neibour_atom_type1 == neibour_atom_type2:
                     count_triplets[(neibour_atom_type1,
-                                mid_atom_type, neibour_atom_type2)] += 1
+                                    mid_atom_type, neibour_atom_type2)] += 1
                 else:
                     count_triplets[(neibour_atom_type1,
-                                mid_atom_type, neibour_atom_type2)] += 1
+                                    mid_atom_type, neibour_atom_type2)] += 1
                     count_triplets[(neibour_atom_type2,
-                                mid_atom_type, neibour_atom_type1)] += 1
+                                    mid_atom_type, neibour_atom_type1)] += 1
 
         count_triplets_renamed = dict()
         for (neibour1, mid, neibour2), count in count_triplets.items():
@@ -87,7 +87,54 @@ class AnalyzeAtom():
         elif res_type == 'dict':
             return self.atoms['type'].value_counts().rename(index=self.atom_type_to_symbol).to_dict()
         else:
-            raise ValueError(f'res_type: {res_type} is not supported. supported res_type : [series, dict]')
+            raise ValueError(
+                f'res_type: {res_type} is not supported. supported res_type : [series, dict]')
+
+    def get_coordination_number_distribution(self, cut_off=0.5, target_atom_type=-1, bond_type='dumpbond', condition=None) -> dict:
+        """
+        配位数の分布を調べる
+        Parameters
+        ----------
+        cut_off : float
+            bond_type == 'dumppos' の時はcut_offの単位はÅ
+            ある原子からcut_off以下の距離にある原子は結合しているとみなす
+
+            bond_type == 'dumpbond' の時はcut_offの単位はbond order
+            ある原子とある原子のbond orderの和がcut_off以上のときに結合しているとみなす
+
+            bond_type == 'dumpbond_cg' の時はcut_offは不必要
+        target_atom_type : int
+            対象となる原子のtype
+        bond_type : str
+            bond_type == 'dumppos' の時はconnect_listはdumpposから生成される
+            bond_type == 'dumpbond' の時connect_listはdumpbondから生成される
+            bond_type == 'dumpbond_cg' の時connect_listはdumpbond_cgから生成される
+
+        condition : function
+            配位数を調べたい原子を指定する関数
+
+        Returns
+        -------
+            coordination_number_distribution: dict
+                配位数の分布
+        """
+        assert target_atom_type in self.get_atom_type_set(), 'invalid target_atom_type.'
+        if condition is None:
+            target_atoms = np.array([True] * self.get_total_atoms())
+        else:
+            target_atoms = condition(self)
+        connect_list = self.get_connect_list(
+            cut_off=cut_off, bond_type=bond_type)
+        coordination_counter = {i: 0 for i in range(9)}
+        atom_types = self.atoms['type'].values
+        for atom_idx in range(self.get_total_atoms()):
+            if not target_atoms[atom_idx]:
+                continue
+            if atom_types[atom_idx] != target_atom_type:
+                continue
+            coordination_counter[len(connect_list[atom_idx])] += 1
+        return coordination_counter
+
 
 class AnalyzeAtomForSDats():
     def __init__():
@@ -155,12 +202,12 @@ class AnalyzeAtomForSDats():
                     neibour_atom_type2 = sdat_atom_type[neibour_atom_idx2]
                     if neibour_atom_type1 == neibour_atom_type2:
                         count_triplets[step_idx][(neibour_atom_type1,
-                                    mid_atom_type, neibour_atom_type2)] += 1
+                                                  mid_atom_type, neibour_atom_type2)] += 1
                     else:
                         count_triplets[step_idx][(neibour_atom_type1,
-                                    mid_atom_type, neibour_atom_type2)] += 1
+                                                  mid_atom_type, neibour_atom_type2)] += 1
                         count_triplets[step_idx][(neibour_atom_type2,
-                                    mid_atom_type, neibour_atom_type1)] += 1
+                                                  mid_atom_type, neibour_atom_type1)] += 1
         df_count_triplets = pd.DataFrame(count_triplets, index=self.step_nums)
 
         def change_column_name(column):
@@ -176,8 +223,59 @@ class AnalyzeAtomForSDats():
         for step_idx in range(len(self.step_nums)):
             count_dict = self.atoms[step_idx]['type'].value_counts().to_dict()
             count_atom_types_list.append(count_dict)
-        df_count_atom_types = pd.DataFrame(data=count_atom_types_list, index=self.step_nums)
-        df_count_atom_types.rename(columns=self.atom_type_to_symbol, inplace=True)
+        df_count_atom_types = pd.DataFrame(
+            data=count_atom_types_list, index=self.step_nums)
+        df_count_atom_types.rename(
+            columns=self.atom_type_to_symbol, inplace=True)
         return df_count_atom_types
 
+    def get_coordination_number_distribution(self, cut_off=0.5, target_atom_type=-1, bond_type='dumpbond', condition=None) -> pd.DataFrame:
+        """
+        配位数の分布を調べる
+        Parameters
+        ----------
+        cut_off : float
+            bond_type == 'dumppos' の時はcut_offの単位はÅ
+            ある原子からcut_off以下の距離にある原子は結合しているとみなす
 
+            bond_type == 'dumpbond' の時はcut_offの単位はbond order
+            ある原子とある原子のbond orderの和がcut_off以上のときに結合しているとみなす
+
+            bond_type == 'dumpbond_cg' の時はcut_offは不必要
+        target_atom_type : int
+            対象となる原子のtype
+        bond_type : str
+            bond_type == 'dumppos' の時はconnect_listはdumpposから生成される
+            bond_type == 'dumpbond' の時connect_listはdumpbondから生成される
+            bond_type == 'dumpbond_cg' の時connect_listはdumpbond_cgから生成される
+
+        condition : function
+            配位数を調べたい原子を指定する関数
+
+        Returns
+        -------
+            df_coordination_number_distribution: pd.DataFrame
+                配位数の分布
+        """
+        assert target_atom_type in self.get_atom_type_set(), 'invalid target_atom_type.'
+        connect_lists = self.get_connect_lists(
+            cut_off=cut_off, bond_type=bond_type)
+        coordination_list = []
+        for step_idx in trange(len(self.step_nums), desc='[counting coordination num]'):
+            if condition is None:
+                target_atoms = np.array([True] * self.get_total_atoms())
+            else:
+                target_atoms = condition(self, step_idx)
+
+            coordination_counter = {i: 0 for i in range(9)}
+            atom_types = self.atoms[step_idx]['type'].values
+            for atom_idx in range(self.get_total_atoms()):
+                if not target_atoms[atom_idx]:
+                    continue
+                if atom_types[atom_idx] != target_atom_type:
+                    continue
+                coordination_counter[len(connect_lists[step_idx][atom_idx])] += 1
+            coordination_list.append(coordination_counter)
+        
+        df_coordination_number = pd.DataFrame(data=coordination_list, index=self.step_nums)
+        return df_coordination_number
