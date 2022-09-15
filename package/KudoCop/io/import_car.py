e426e25fbed4540c2cc535519012f2db45875e35
kudoryutaro <ryutaro.kudo.p2@dc.tohoku.ac.jp>
2022-09-15 12:04:38 +0900

    Fix import_car.py to read cell size

--- package/KudoCop/io/import_car.py
+++ package/KudoCop/io/import_car.py
@@ -11,6 +11,9 @@ class ImportCar():
         df = pd.read_csv(ifn, names=['symbol+id', 'x', 'y', 'z', 'XXXX', '1', 'xx', 'symbol', '0.000'],
                          usecols=['x', 'y', 'z', 'symbol'],
                          skiprows=4,  sep='\s+')
+        self.cell[0] = float(df.iloc[0, 0])
+        self.cell[1] = float(df.iloc[0, 1])
+        self.cell[2] = float(df.iloc[0, 2])
         df['type'] = df['symbol'].map(self.atom_symbol_to_type)
         df.drop('symbol', axis=1, inplace=True)
         df.dropna(inplace=True)
