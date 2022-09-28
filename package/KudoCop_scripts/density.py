2e17d15a87dee59ae181c3e9c3c98578057c1a67
kudoryutaro <ryutaro.kudo.p2@dc.tohoku.ac.jp>
2022-09-28 17:09:34 +0900

    added description of kudocop scripts

--- package/KudoCop_scripts/density.py
+++ package/KudoCop_scripts/density.py
@@ -31,39 +31,48 @@ if __name__ == '__main__':
                         help='max z')
 
     args = parser.parse_args()
-
+    assert args.file_name is not None, 'set file_name'
     sdat = SimulationDat()
     sdat.import_para(args.para_file_name)
     import_file_type = analyze_file_type(args.file_name)
     sdat.import_file(args.file_name, import_file_type)
 
     if args.x_max is None:
-        x_max = sdat.atoms['x'].max()
+        if sdat.cell[0] is not None:
+            x_max = sdat.cell[0]
+        else:
+            x_max = sdat.atoms['x'].max()
     else:
         x_max = args.x_max
     
     if args.y_max is None:
-        y_max = sdat.atoms['y'].max()
+        if sdat.cell[1] is not None:
+            y_max = sdat.cell[1]
+        else:
+            y_max = sdat.atoms['y'].max()
     else:
         y_max = args.y_max
     
     if args.z_max is None:
-        z_max = sdat.atoms['z'].max()
+        if sdat.cell[2] is not None:
+            z_max = sdat.cell[2]
+        else:
+            z_max = sdat.atoms['z'].max()
     else:
         z_max = args.z_max
         
     if args.x_min is None:
-        x_min = sdat.atoms['x'].min()
+        x_min = 0
     else:
         x_min = args.x_min
     
     if args.y_min is None:
-        y_min = sdat.atoms['y'].min()
+        y_min = 0
     else:
         y_min = args.y_min
     
     if args.z_min is None:
-        z_min = sdat.atoms['z'].min()
+        z_min = 0
     else:
         z_min = args.z_min
 
@@ -71,4 +80,4 @@ if __name__ == '__main__':
     print(f'condition: {x_min} <= x <= {x_max}')
     print(f'condition: {y_min} <= y <= {y_max}')
     print(f'condition: {z_min} <= z <= {z_max}')
-    print(f'density : {density}')
+    print(f'density (g/cm^3): {density}')
